14369b69d224d79c4876d1742c6741b0c1ee3eb6
Arlet2 <shulga@niuitmo.ru>
2023-06-20 15:59:08 +0300

    Add progress info entity

--- src/main/java/space/arlet/meowhack/data/ProgressInfo.java
+++ src/main/java/space/arlet/meowhack/data/ProgressInfo.java
@@ -1,4 +1,39 @@
 package space.arlet.meowhack.data;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="user_progress")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProgressInfo {
+    @Id
+    private long userId;
+
+    private long expEco;
+    private long lvlEco;
+
+    private long expFit;
+    private long lvlFit;
+
+    private long expFriendly;
+    private long lvlFriendly;
+
+    private long expHealthy;
+    private long lvlHealthy;
+
+    private long expOpen;
+    private long lvlOpen;
+
+    private long expPro;
+    private long lvlPro;
 }
