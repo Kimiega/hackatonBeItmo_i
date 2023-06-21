304a852a230bacf223bb17719f390ea93eb3e28a
Arlet2 <shulga@niuitmo.ru>
2023-06-21 21:15:41 +0300

    Add endpoint for getting all achievs

--- back/src/main/java/space/arlet/meowhack/controllers/AchievementsController.java
+++ back/src/main/java/space/arlet/meowhack/controllers/AchievementsController.java
@@ -1,17 +1,31 @@
 package space.arlet.meowhack.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import space.arlet.meowhack.data.UserAchievementInfo;
+import space.arlet.meowhack.services.UserNotFoundException;
+import space.arlet.meowhack.services.achievements.AchievementsService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value="${api_path}/achieves")
+@RequestMapping(value="${api_path}/achievements")
 public class AchievementsController {
 
-    @RequestMapping(value="/test", method = RequestMethod.GET)
-    public ResponseEntity<String> test(int a) {
-        return new ResponseEntity<>((a+1)+"", HttpStatus.OK);
+    private final AchievementsService achievementsService;
+
+    @Autowired
+    AchievementsController(AchievementsService achievementsService) {
+        this.achievementsService = achievementsService;
     }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserAchievementInfo>> getAllAchievements(long userId) {
+        return new ResponseEntity<>(achievementsService.getAllAchievementsByUserId(userId), HttpStatus.OK);
+    }
+
 }
