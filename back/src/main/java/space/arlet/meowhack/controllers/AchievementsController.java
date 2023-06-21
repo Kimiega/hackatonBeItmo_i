f0d21f24e494df3e7da980299e7e01061a29d049
Arlet2 <shulga@niuitmo.ru>
2023-06-22 02:51:29 +0300

    Add getting by type

--- back/src/main/java/space/arlet/meowhack/controllers/AchievementsController.java
+++ back/src/main/java/space/arlet/meowhack/controllers/AchievementsController.java
@@ -1,16 +1,23 @@
 package space.arlet.meowhack.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import space.arlet.meowhack.data.Achievement;
 import space.arlet.meowhack.data.UserAchievementInfo;
+import space.arlet.meowhack.services.Direction;
 import space.arlet.meowhack.services.achievements.AchievementNotFoundException;
 import space.arlet.meowhack.services.achievements.AchievementExistsException;
 import space.arlet.meowhack.services.achievements.AchievementsService;
+import space.arlet.meowhack.services.progress.ProgressService;
 
 import java.util.List;
 
@@ -39,12 +46,34 @@ public class AchievementsController {
     }
 
     @RequestMapping(value = "${api_path}/achievement", method = RequestMethod.POST)
-    public ResponseEntity<Void> addAchievement(Achievement achievement) {
+    public ResponseEntity<Void> addAchievement(@RequestBody AchievementRequestBody requestBody) {
         try {
+            var builder = new Achievement.Builder();
 
+            Achievement achievement = builder
+                    .setExpByTier(requestBody.tier)
+                    .setTitle(requestBody.title)
+                    .setDescription(requestBody.description)
+                    .setImageUrl(requestBody.imageURL)
+                    .setDirection(requestBody.direction)
+                    .build();
+
+            achievementsService.createAchievement(achievement);
+            return new ResponseEntity<>(null, HttpStatus.OK);
         } catch (AchievementExistsException e) {
-
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class AchievementRequestBody {
+        private String title;
+        private String description;
+        private String imageURL;
+        private Direction direction;
+        private ProgressService.Tier tier;
     }
 
 }
