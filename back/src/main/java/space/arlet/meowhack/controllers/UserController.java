fdcc997c4cf2f310fa02af1f59100daaedec266d
Kimiega <dmbelitmo@mail.ru>
2023-06-21 21:55:39 +0300

    update user service

---
+++ back/src/main/java/space/arlet/meowhack/controllers/UserController.java
@@ -0,0 +1,9 @@
+package space.arlet.meowhack.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "${api_path}/users")
+public class UserController {
+}
