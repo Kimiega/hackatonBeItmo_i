a75799eadecb8cd506a08aafced9f277cb3e29c6
Arlet2 <shulga@niuitmo.ru>
2023-06-21 16:49:52 +0300

    Add progress controller

--- back/src/main/java/space/arlet/meowhack/controllers/ProgressController.java
+++ back/src/main/java/space/arlet/meowhack/controllers/ProgressController.java
@@ -1,9 +1,32 @@
 package space.arlet.meowhack.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import space.arlet.meowhack.data.ProgressInfo;
+import space.arlet.meowhack.services.UserNotFoundException;
+import space.arlet.meowhack.services.progress.ProgressService;
 
 @RestController
 @RequestMapping(value = "${api_path}/progress")
 public class ProgressController {
+
+    private final ProgressService progressService;
+
+    @Autowired
+    ProgressController(ProgressService progressService) {
+        this.progressService = progressService;
+    }
+
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public ResponseEntity<ProgressInfo> getInfo(long userId) {
+        try {
+            return new ResponseEntity<>(progressService.getProgressInfoByUserId(userId), HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
