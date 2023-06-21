fbdcc4b84172e2e1e84647880bae1b021ed41d44
Arlet2 <shulga@niuitmo.ru>
2023-06-21 16:26:50 +0300

    Add builder

--- back/src/main/java/space/arlet/meowhack/data/Achiev.java
+++ back/src/main/java/space/arlet/meowhack/data/Achiev.java
@@ -1,14 +1,13 @@
 package space.arlet.meowhack.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import space.arlet.meowhack.services.Direction;
 import space.arlet.meowhack.services.progress.ExperienceSource;
+import space.arlet.meowhack.services.progress.ProgressService;
 
 @Entity
 @Table(name = "user_achievements")
@@ -20,12 +19,13 @@ import space.arlet.meowhack.services.progress.ExperienceSource;
 @Setter
 public class Achiev implements ExperienceSource {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;
     private String description;
 
-    private float receivingPercent;
+    private int ownersCount;
 
     private String imageURL;
 
@@ -41,5 +41,41 @@ public class Achiev implements ExperienceSource {
     @Override
     public Direction getDirection() {
         return direction;
+    }
+
+    public class Builder {
+        private final Achiev currentAchiev;
+
+        Builder() {
+            currentAchiev = new Achiev();
+        }
+        public Achiev build() {
+            return currentAchiev;
+        }
+
+        public Builder setTitle(String value) {
+            currentAchiev.setTitle(value);
+            return this;
+        }
+
+        public Builder setDescription(String value) {
+            currentAchiev.setDescription(value);
+            return this;
+        }
+
+        public Builder setImageUrl(String value) {
+            currentAchiev.setImageURL(value);
+            return this;
+        }
+
+        public Builder setDirection(Direction value) {
+            currentAchiev.setDirection(value);
+            return this;
+        }
+
+        public Builder setExpByTier(ProgressService.Tier tier) {
+            currentAchiev.setExp(tier.exp);
+            return this;
+        }
     }
 }
